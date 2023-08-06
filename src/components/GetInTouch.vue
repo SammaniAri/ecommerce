6c5ef6c3ddd7634ecfb47703a9d07cab292ccc41
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-08-06 18:13:17 +0200

    made contact section responsive

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -1,26 +1,22 @@
 <!-- @format -->
-
 <template>
-	<div
-		id="contact"
-		class="d-flex justify-center flex-column">
-		<v-sheet
-			class="background d-flex flex-column justify-center pa-6"
-			><p
-				class="text-h5 py-1 d-flex pl-15 pt-10">
-				STUDIO MOONE</p
-			>
-		</v-sheet>
-
-		<div class="d-flex">
-			<v-sheet
-				class="section-height px-6 w-25">
-				<p
-					class="text-h3 py-1 d-flex pl-14">
-					Get in Touch</p
-				>
-			</v-sheet>
-			<v-sheet
+	<v-container class="container">
+		<p
+			class="get-in-touch text-h5 py-1 d-flex pl-15 pt-10">
+			STUDIO MOONE</p
+		>
+		<v-row no-gutters>
+			<v-col
+				cols="12"
+				sm="6"
+				md="3"
+				class="get-in-touch text-h3 py-1 d-flex pl-14">
+				Get In Touch
+			</v-col>
+			<v-col
+				cols="12"
+				sm="6"
+				md="3"
 				class="section-height px-6 w-25">
 				<p class="d-flex justify-center"
 					>Mailing Address</p
@@ -29,35 +25,43 @@
 					class="d-flex justify-center">
 					123 Anywhere St., Any City,
 					State, Country 12345
-				</p></v-sheet
-			>
-			<v-sheet
-				class="section-height w-25">
+				</p>
+			</v-col>
+			<v-col
+				cols="12"
+				sm="6"
+				md="3"
+				class="section-height px-6 w-25">
 				<p class="d-flex justify-center"
 					>Email Address</p
 				><p
 					class="d-flex justify-center"
 					>hello@reallygreatsite.com</p
-				></v-sheet
-			>
-			<v-sheet
-				class="section-height w-25">
+				>
+			</v-col>
+			<v-col
+				cols="12"
+				sm="6"
+				md="3"
+				class="section-height px-6 w-25">
 				<p class="d-flex justify-center"
 					>Phone Number</p
 				><p
 					class="d-flex justify-center"
 					>(123) 456 7890</p
-				></v-sheet
-			>
-		</div>
-	</div>
+				>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 <style scoped>
 .section-height {
-	height: 10rem;
 	background-color: #ece6d6;
 }
-.background {
+.container {
+	max-width: 2000px;
+}
+.get-in-touch {
 	background-color: #ece6d6;
 }
 </style>
